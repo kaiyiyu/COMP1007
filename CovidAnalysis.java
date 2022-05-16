e98dd433deafc6f72c29f7bda4cb501063e32d3d
Kai-Yu <kaiyu2871@gmail.com>
2022-05-16 22:36:00 +0800

    first and second menu

--- CovidAnalysis.java
+++ CovidAnalysis.java
@@ -2,30 +2,13 @@
  * Author: Kai-Yu Yu
  * Purpose: Implementation of File IO methods and the main method used for the overall program
  * Date: 14/05/2022
- * Remarks: Methods are all under 50 lines only (except main). Code comments made the methods 
- *          seem long
  */
 
  // main -> menu system
  // fileIO
 
 /* 
-System.out.println( "**************************************************************************\n" +
-                    "|   Welcome to the JRC COVID-19 Analysis Program. There are a total of   |\n" +
-                    "|  ‘XYZ’ records loaded. Please make a selection from the Menu below to  |\n" +
-                    "|                 choose which area (or date) to analyse:                |\n" +
-                    "**************************************************************************\n\n" +
-                    "> 1. All countries\n" +
-                    "> 2. Countries in South America\n" +
-                    "> 3. Countries in North America\n" +
-                    "> 4. Countries in Oceania\n" +
-                    "> 5. Countries in Asia\n" +
-                    "> 6. Countries in Africa\n" +
-                    "> 7. Countries in Europe\n" +
-                    "> 8. Enter a Country\n" +
-                    "> 9. Enter a Date\n" +
-                    "> 10. Exit the Program\n" +
-                    "Enter selection: " );    
+
 
 System.out.println( "\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n" +
                     "Please select from a statistic below:\n" +
@@ -37,15 +20,39 @@ System.out.println( "\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
                     "> 6. Number and percentage of cumulatively positive cases deceased\n" +
                     "> 7. All of the above statistics\n" +
                     "Enter selection: " );
+                selection = input.nextInt();
+
+                switch (selection)
+                {
+                    case 1:
+                    break;
+                    case 2;
+                    break;
+                    case 3;
+                    break;
+                    case 4;
+                    break;
+                    case 5;
+                    break;
+                    case 6;
+                    break;
+                    case 7;
+                    break;
+                    default: // Default to re-enter a menu choice from 1 to 7
+                        System.out.println("Invalid selection. Please enter a number between 1 and 7 inclusive.");
                     */
 import java.io.*;
 import java.util.*;
 public class CovidAnalysis
 {
+    /**
+     * METHOD: main
+     * PURPOSE: The main method to run the JRC COVID-19 data analysis program
+     */
     public static void main(String[] arg)
     {
         String fileName = "jrc-covid-19-all-days-of-world_ASSIGNMENT-FIXED.csv";
-        int numOfRecords;
+        int numOfRecords, selection;
         
         // Open file for reading and get the number of records for array size
         numOfRecords = readFile(fileName);
@@ -54,11 +61,142 @@ public class CovidAnalysis
         CovidRecord[] recordsArray = new CovidRecord[numOfRecords];
         loadData(fileName, recordsArray);
 
-        for (int i = 0; i < recordsArray.length; i++)
+        // Exception handling for invalid data type and integer greater than max integer value in Java 
+        // during menu option selection.   
+        try 
         {
-            System.out.println(recordsArray[i].toString());
+            Scanner input = new Scanner(System.in);
+            // Do-while loop to repeat program until exit condition (user chooses exit option 10)
+            do
+            {
+                System.out.printf("**************************************************************************\n" +
+                                  "|    Welcome to the JRC COVID-19 Analysis Program. There are a total of   |\n" +
+                                  "|  ‘%d’ records loaded. Please make a selection from the Menu below to  |\n" +
+                                  "|                  choose which area (or date) to analyse:                |\n" +
+                                  "**************************************************************************\n\n" +
+                                  "> 1. All countries\n" +
+                                  "> 2. Countries in South America\n" +
+                                  "> 3. Countries in North America\n" +
+                                  "> 4. Countries in Oceania\n" +
+                                  "> 5. Countries in Asia\n" +
+                                  "> 6. Countries in Africa\n" +
+                                  "> 7. Countries in Europe\n" +
+                                  "> 8. Enter a Country\n" +
+                                  "> 9. Enter a Date\n" +
+                                  "> 10. Exit the Program\n" +
+                                  "Enter selection: ", numOfRecords); 
+                selection = input.nextInt();
+                
+                switch (selection)
+                {
+                    case 1:
+                        promptSecondMenu(input, recordsArray);
+                    break;
+                    case 2: case 3: case 4: case 5: case 6: case 7:
+                    break;
+                    case 8:
+                    break;
+                    case 9:
+                    break;
+                    case 10:
+                        System.out.println("\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=" +
+                                           "\nThank you for using the JRC COVID-19 Analysis Program!");
+                    break;
+                    default: // Default to re-enter a menu choice from 1 to 10
+                        System.out.println("Invalid selection. Please re-enter a number from 1 to 10.\n");
+                    break;
+                }   
+            }   while (selection != 10);    
+            input.close();
         }
-    }
+        catch (InputMismatchException exception) // Program will display a message and terminate after an incorrect input
+        {
+            System.out.println("\nInvalid input: " + exception +
+                               "\nProgram will stop executing. Goodbye!");
+        }
+    }   // End main
+
+    /**
+     * METHOD: promptSecondMenu
+     * IMPORT: pSelection (int), pRecordsArray (CovidRecord[])
+     * EXPORT: none
+     * ASSERTION: Prompts user for a second menu selection and calls the appropriate method
+     */
+    private static void promptSecondMenu(Scanner pInput, CovidRecord[] pRecordsArray)
+    {
+        int secondSelection;
+        boolean validSelection = true;
+        // Exeption handling for non-integer user input
+        try
+        {   
+            // Do-while loop to keep asking for valid menu selection if user inputs integer out of range
+            do 
+            {
+                System.out.println("\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n" +
+                                   "Please select from a statistic below:\n" +
+                                   "> 1. Total number of cumulatively positive cases\n" +
+                                   "> 2. Total number of cumulatively deceased cases\n" +
+                                   "> 3. Total number of cumulatively recovered cases\n" +
+                                   "> 4. Average daily number of currently positive cases\n" +
+                                   "> 5. Number and percentage of cumulatively positive cases recovered\n" +
+                                   "> 6. Number and percentage of cumulatively positive cases deceased\n" +
+                                   "> 7. All of the above statistics\n" +
+                                   "Enter selection: ");
+                secondSelection = pInput.nextInt();
+
+                // Switch statement for second menu selection
+                switch (secondSelection)
+                {
+                    case 1:
+                        System.out.println("\n===========================================" +
+                                           "\nTotal number of cumulatively positive cases" +
+                                           "\n===========================================");
+                    break;
+                    case 2:
+                        System.out.println("\n===========================================" +
+                                           "\nTotal number of cumulatively deceased cases" +
+                                           "\n===========================================");
+                    break;
+                    case 3:
+                        System.out.println("\n============================================" +
+                                           "\nTotal number of cumulatively recovered cases" +
+                                           "\n============================================");
+                    break;
+                    case 4:
+                        System.out.println("\n================================================" +
+                                           "\nAverage daily number of currently positive cases" +
+                                           "\n================================================");
+                    break;
+                    case 5:
+                        System.out.println("\n===============================================" +
+                                           "\n     Number and percentage of cumulatively     " + 
+                                           "\n            positive cases recovered           " +
+                                           "\n===============================================");
+                    break;
+                    case 6:
+                        System.out.println("\n===============================================" +
+                                           "\n     Number and percentage of cumulatively     " + 
+                                           "\n            positive cases deceased            " +
+                                           "\n===============================================");
+                    break;
+                    case 7:
+                        System.out.println("\n=====================================" +
+                                           "\n     All of the above statistics     " +
+                                           "\n=====================================");
+                    break;
+                    default: // Default to re-enter a menu choice from 1 to 7
+                        System.out.println("Invalid selection. Please re-enter a number from 1 to 7.\n");
+                        validSelection = false;
+                    break;
+                }   
+            }   while (!(validSelection));
+        }
+        catch (InputMismatchException exception) // Program will display a message and terminate after an incorrect input
+        {
+            System.out.println("\nInvalid input: " + exception +
+                               "\nProgram will stop executing. Goodbye!");
+        }
+    }  // End promptSecondMenu
 
     /**
      * METHOD: readFile
@@ -105,7 +243,7 @@ public class CovidAnalysis
             System.out.println("ERROR " + errorDetails.getMessage());
         }
         return lineNum;
-    }
+    }   // End readFile
 
     /**
      * METHOD: loadData
@@ -153,7 +291,7 @@ public class CovidAnalysis
             }
             System.out.println("ERROR " + errorDetails.getMessage());
         }
-    }
+    }   // End loadData
 
     /**
      * METHOD: processLine
@@ -207,5 +345,6 @@ public class CovidAnalysis
         
         // Initialise the new CovidRecord object to the array of CovidRecord objects
         pRecordsArray[pLineNum] = record;
-    }
+    }   // End processLine
+
 } // End of CovidAnalysis class
